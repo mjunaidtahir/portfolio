32fac26911c646ab31f2fc2e8fcc023304032294
Muhammad Junaid Tahir <mjunaidtahir@renesistech.com>
2023-04-04 12:16:28 +0500

    created folio project

--- lib/utilities/showcase_app_model.dart
+++ lib/utilities/showcase_app_model.dart
@@ -31,48 +31,48 @@ class ShowcaseAppModel {
 
 // List of apps that will be listed on landing page.
 const apps = [
-  ShowcaseAppModel.withLocalAsset(
+  ShowcaseAppModel.withNetworkAsset(
     name: 'Beast Mode Soccer+',
-    image: 'assets/images/beast.png',
+    image: 'https://iili.io/HOuqa8g.webp',
     playStoreURL:
         'https://play.google.com/store/apps/details?id=com.akkastech.bmsu.bmsu&hl=en&gl=US',
     appStoreURL: 'https://apps.apple.com/us/app/beast-mode-soccer/id1398275434',
     topic: 'Suports and Soccer',
   ),
-  ShowcaseAppModel.withLocalAsset(
+  ShowcaseAppModel.withNetworkAsset(
     name: 'Confidentials Predrinks',
-    image: 'assets/images/confi.png',
+    image: 'https://iili.io/HOuq7F1.webp',
     playStoreURL:
         'https://play.google.com/store/apps/details?id=com.predrinks.app&hl=en&gl=US',
     appStoreURL: 'https://apps.apple.com/pk/app/confidentials/id1230985375',
     topic: 'Buy Vouchers and Redeem it',
   ),
-  ShowcaseAppModel.withLocalAsset(
+  ShowcaseAppModel.withNetworkAsset(
     name: 'PreDrinks Scanner',
-    image: 'assets/images/scanner.png',
+    image: 'https://iili.io/HOuql9a.webp',
     playStoreURL:
         'https://play.google.com/store/apps/details?id=com.predrinks.scannerapp&hl=en_US&gl=US',
     appStoreURL:
         'https://apps.apple.com/gb/app/confidentials-scanner/id1234556922',
     topic: 'Buy Vouchers and Redeem it',
   ),
-  ShowcaseAppModel.withLocalAsset(
+  ShowcaseAppModel.withNetworkAsset(
     name: 'Employe Evaluation',
-    image: 'assets/images/eep.png',
+    image: 'https://iili.io/HOuqR6P.png',
     playStoreURL: '',
     appStoreURL: '',
     topic: 'Evaluaiton of Employees',
   ),
-  ShowcaseAppModel.withLocalAsset(
+  ShowcaseAppModel.withNetworkAsset(
     name: 'My Nikah ',
-    image: 'assets/images/nika.png',
+    image: 'https://iili.io/HOuqYcF.png',
     playStoreURL: '',
     appStoreURL: '',
     topic: 'Nikah and Marriage',
   ),
-  ShowcaseAppModel.withLocalAsset(
+  ShowcaseAppModel.withNetworkAsset(
     name: 'ERP PACK',
-    image: 'assets/images/erp.png',
+    image: 'https://iili.io/HOuqAMB.webp',
     playStoreURL:
         'https://play.google.com/store/apps/details?id=com.codility.erp_pack',
     appStoreURL: '',
